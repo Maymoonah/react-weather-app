e800f8b6d16777618b0e7290b0b2f70ce0b0d54a
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2021-03-17 22:20:19 -0700

    add constructor and state and componentDidMount with API call.

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import ReactDOM from "react-dom"''
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: 34.0522,
+      lon: 118.2437,
+      API_KEY: "a477dcbfc72c1c3cfb42a0aa6bacccbd"
+    };
+    // Binding method
+    
+  }
+
+  componentWillMount() {
+    let api_call = `https://api.openweathermap.org/data/2.5/onecall?lat=${this.state.latitude}&lon=${this.state.longitude}&appid=${this.state.API_KEY}`;
+    if(this.state.lat) {
+      fetch(api_call)
+    .then(response => response.json())
+    .then(data => console.log(data));
+    }
+    
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+        <h1>Hi</h1>
+        </header>
+      </div>
+    );
+    
+  }
 }
 
 export default App;
