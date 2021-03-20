c80bee7f46ac90af050fd152b68ad73e0bd3df3f
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2021-03-19 18:26:34 -0700

    editing fetch

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -5,33 +5,30 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoading: false,
-      lat: 34.0522,
-      lon: 118.2437,
-      API_KEY: "a477dcbfc72c1c3cfb42a0aa6bacccbd",
+      city: "Los Angeles",
       data: []
     };
     
   }
 
-  getAPI() {
-    let api_call = '';
-    
-    if(typeof this.props.count !== 'undefined') {
-      api_call = `https://api.openweathermap.org/data/2.5/onecall?lat=${this.state.latitude}&lon=${this.state.longitude}&appid=a477dcbfc72c1c3cfb42a0aa6bacccbd`;
-    }
-
-    fetch(api_call)
-    .then(response => response.json())
-    .then(data => this.setState({ data: data, isLoading: false }));
+  componentDidMount() {
+    fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=Los%20Angeles&lat=0&lon=0&callback=test&id=2172797&lang=null&units=%22metric%22%20or%20%22imperial%22&mode=xml%2C%20html", {
+  "method": "GET",
+  "headers": {
+    "x-rapidapi-key": "c617e14926msh1b32a202714298ap130120jsn5a3fce0740f8",
+    "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
+  }
+  })
+  .then(response => {
+    console.log(response);
+  })
+  .catch(err => {
+    console.error(err);
+  });
   }
 
-  componentWillMount() {
-    this.getAPI();
-  }
   
   render () {
-    console.log(this.state.data);
     return (
       <div className="App">
         <header className="App-header">
