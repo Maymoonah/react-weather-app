7d0e10ef030b5b1f2b4b316a665b39ec5f521458
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2021-03-19 22:52:38 -0700

    finally fixed the pi call

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -5,35 +5,23 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      city: "Los Angeles",
-      data: []
+      city: "London",
+      data: [],
+      api_key: 'dbcb6d54de23ada0919e45a242572082'
     };
     
   }
 
   componentDidMount() {
-    fetch("https://community-open-weather-map.p.rapidapi.com/weather?q=Los%20Angeles&lat=0&lon=0&callback=test&id=2172797&lang=null&units=%22metric%22%20or%20%22imperial%22&mode=xml%2C%20html", {
-  "method": "GET",
-  "headers": {
-    "x-rapidapi-key": "c617e14926msh1b32a202714298ap130120jsn5a3fce0740f8",
-    "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
-  }
-  })
-  .then(response => {
-    console.log(response);
-  })
-  .catch(err => {
-    console.error(err);
-  });
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&APPID=${this.state.api_key}`) 
+  .then(response => response.json())
+  .then(data => { console.log(data) });
   }
 
   
   render () {
     return (
       <div className="App">
-        <header className="App-header">
-        <h1>Hi</h1>
-        </header>
       </div>
     );
     
