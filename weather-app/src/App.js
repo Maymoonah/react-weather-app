921436ad6897e71de38a44d5c44e738675cb6908
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2021-03-20 19:22:07 -0700

    revert to older version of App.js

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      city: "",
+      city: "Los Angeles",
       country: "",
       lat: "",
       lon: "",
@@ -42,33 +42,35 @@ class App extends Component {
       weatherDescription: data.weather[0].description,
       wind: data.wind.speed, 
       humidity: data.main.humidity
-    })).catch(err => console.log("Loading..."));
-    this.renderInfo();
+    }));
+    if(typeof this.state.country !== undefined) {
+      this.renderInfo();
+    }
   }
 
   renderInfo() {
     document.getElementsByClassName("info")[0].innerHTML = `
-    <ul>
-        <li>City: ${this.state.city}, ${this.state.country}</li>
-        <li>Latitude: ${this.state.lat}</li>
-        <li>Longitude: ${this.state.lon}</li>
-        <li>Temperature: ${this.state.temperature}</li>
-        <li>Feels Like: ${this.state.feelsLike}</li>
-        <li>Maximum Temperature: ${this.state.maxTemp}</li>
-        <li>Minimum Temperature: ${this.state.minTemp}</li>
-        <li>Weather: ${this.state.weather}</li>
-        <li>Weather Description: ${this.state.weatherDescription}</li>
-        <li>Humidity: ${this.state.humidity}</li>
-        <li>Wind Speed: ${this.state.wind} mph</li>
+    <ul className="weather">
+        <li className="weatherInfo">City: ${this.state.city}, ${this.state.country}</li>
+        <li className="weatherInfo">Latitude: ${this.state.lat}</li>
+        <li className="weatherInfo">Longitude: ${this.state.lon}</li>
+        <li className="weatherInfo">Temperature: ${this.state.temperature}</li>
+        <li className="weatherInfo">Feels Like: ${this.state.feelsLike}</li>
+        <li className="weatherInfo">Maximum Temperature: ${this.state.maxTemp}</li>
+        <li className="weatherInfo">Minimum Temperature: ${this.state.minTemp}</li>
+        <li className="weatherInfo">Weather: ${this.state.weather}</li>
+        <li className="weatherInfo">Weather Description: ${this.state.weatherDescription}</li>
+        <li className="weatherInfo">Humidity: ${this.state.humidity}</li>
+        <li className="weatherInfo">Wind Speed: ${this.state.wind} mph</li>
     </ul>`
   }  
 
   render () {
     return (
       <div className="App">
-        <h1>Weather Application</h1>
-        <input type="text" className="city"/>
-        <button type="submit" onClick={this.getCity}>Get City</button>
+        <h1 id="title">Weather App</h1>
+        <input type="text" placeholder="Enter city..." className="city"/>
+        <button id="submit" type="submit" onClick={this.getCity}>Check Weather!</button>
         <div className="info"></div>
       </div> 
     );
