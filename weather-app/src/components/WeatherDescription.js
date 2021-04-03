9ae9a5b31fdbde55f44fb06d8404c8bc7c577679
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2021-04-03 13:44:02 -0700

    created components folder and new WeatherDescription component

---
+++ weather-app/src/components/WeatherDescription.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class WeatherDescription extends Component {
+  render() {
+    return (
+    	<div>Hi, I'm the WeatherDescription Component</div>
+    )
+  }
+}
+
+export default WeatherDescription;
