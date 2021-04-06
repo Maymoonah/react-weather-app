885ca7542892af03445679758c89792ab50aa27e
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2021-04-05 23:24:45 -0700

    created new component Images

---
+++ weather-app/src/components/Images.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Images extends Component {
+  render() {
+    return (
+		<div className="images">
+			Hi, I'm Images!
+		</div>
+		)
+	}
+}
+
+export default Images;
